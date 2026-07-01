5db539bbfce09c77229b5717cc8f1b5fdb5a8f13
SehajveerSingh2005 <sehajveersingh2005@gmail.com>
2026-07-01 14:55:37 +0530

    fix: disable browser default context menu in dock window

--- src/dock-main.tsx
+++ src/dock-main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import Dock from './Dock'
 import './Dock.css'
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Dock />
